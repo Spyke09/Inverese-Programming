e28c596687b9f42cf86bd756bd1ce5cd330922f5
Spyke09 <romanov.alexanderromanov@yandex.ru>
2024-03-29 23:54:53 +0300

    some changes

--- unique_bilevel_programming_cplex/visualization/main.py
+++ unique_bilevel_programming_cplex/visualization/main.py
@@ -1,15 +1,11 @@
 import json
 from datetime import datetime
-
-import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 import unique_bilevel_programming_cplex.src.egm.data_parser as data_parser
 from unique_bilevel_programming_cplex.src.base.common import is_lp_nan
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-
-
 
 if __name__ == "__main__":
     def visual(year, json_path, countries):
@@ -17,40 +13,56 @@ if __name__ == "__main__":
         parser = data_parser.DataParser(dates)
         data = parser.get_data()
 
-        all_nodes = set(j for c_1, ccc in data.export_assoc.items() for j in ccc)
-        result = dict()
-        edges = set()
-        nodes = set()
+        all_countries = data.cc_list_full
+        all_lng = data.graph_db["lngList"]
+        export_result = {"flow_pred": dict(), "flow_true": dict()}
+        lng_result = {"flow_pred": dict(), "flow_true": dict()}
         with open(json_path, "r") as f:
             result_data = json.load(f)
             for edge_name, flow in result_data["x"].items():
                 if "flowMid" in edge_name:
                     _, date, c_1, c_2 = edge_name.split("_")
                     date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
-                    c_1 = c_1.replace("export ", "")
-                    cc_12 = c_1, c_2
-                    if date not in result:
-                        result[date] = dict()
-                    result[date][cc_12] = flow
-                    nodes.update(cc_12)
-                    edges.add(cc_12)
+                    if "sosExport" in c_1 and "sosGetExport" in c_2:
+                        c_1 = c_1.replace(" sosExport", "")
+                        c_2 = c_2.replace(" sosGetExport", "")
+                        export_result["flow_pred"][date, c_1, c_2] = flow
+                        export_result["flow_true"][date, c_1, c_2] = data.export_assoc[c_1][c_2][date]
+
+                    if c_1 in all_lng and c_2 == f"{c_1} sos":
+                        lng_result["flow_pred"][date, c_1] = flow
+                        lng_result["flow_true"][date, c_1] = data.terminal_db[c_1]["MonthData"][date]["sendOut"]
+
+        export_df = pd.DataFrame(export_result)
+        export_df["flow_true"] /= 1e5
+        export_df["flow_pred"] /= 1e5
+        export_df.reset_index(names=["date", "c_1", "c_2"], inplace=True)
+        export_df.sort_values("date", inplace=True)
+        export_df.to_csv("export_df.csv", sep=";")
+
+        lng_df = pd.DataFrame(lng_result)
+        lng_df["flow_true"] /= 1e5
+        lng_df["flow_pred"] /= 1e5
+        lng_df.reset_index(names=["date", "c_1"], inplace=True)
+        lng_df.sort_values("date", inplace=True)
+        lng_df.to_csv("lng_df.csv", sep=";")
 
         cc_list = list(countries)
         for_plot_1_true = [
-            sum(c[d] for d in result for c in data.export_assoc[cc].values() if not is_lp_nan(c[d]))
-            for cc in cc_list
+            export_df["flow_true"][export_df["c_1"] == c_1].sum()
+            for c_1 in cc_list
         ] + [
-            sum(tt["MonthData"][d]["sendOut"] for d in result for tt in data.terminal_db.values())
+            lng_df["flow_true"].sum()
         ]
         for_plot_1_pred = [
-            sum(result[d][cc, c] for d in result for c in nodes if (cc, c) in result[d])
-            for cc in cc_list
+            export_df["flow_pred"][export_df["c_1"] == c_1].sum()
+            for c_1 in cc_list
         ] + [
-            sum(result[d][rd] for d in result for rd in result[d] if rd[0] in data.terminal_db)
+            lng_df["flow_pred"].sum()
         ]
+        print(for_plot_1_true)
+        print(for_plot_1_pred)
 
-        print(for_plot_1_true[-1])
-        print(for_plot_1_pred[-1])
 
         cc_list += ["СПГ"]
         barWidth = 0.25
@@ -62,37 +74,37 @@ if __name__ == "__main__":
 
         for i, cc in enumerate(br1):
             per = 200 * (for_plot_1_true[i] - for_plot_1_pred[i]) / (for_plot_1_true[i] + for_plot_1_pred[i])
-            plt.text(cc, barWidth + for_plot_1_pred[i], s=f'{per:.1f}%')
+            h = max(for_plot_1_pred[i], for_plot_1_true[i])
+            plt.text(cc, barWidth + h, s=f'{per:.1f}%')
 
         plt.legend()
         plt.show()
 
-        dates = [i for i in dates if i in result]
-        for_plot_2_pred = {
-            cc: [
-                sum(result[d][cc, c] for c in nodes if (cc, c) in result[d])
-                for d in dates
-            ]
-            for cc in cc_list[:-1]
-        }
+        dates = [i for i in dates if i in export_df["date"].unique()]
+
         for_plot_2_true = {
-            cc: [
-                sum(c[d] for c in data.export_assoc[cc].values() if not is_lp_nan(c[d]))
-                for d in dates
-            ]
-            for cc in cc_list[:-1]
+            c_1: [export_df["flow_true"][(export_df["c_1"] == c_1) & (export_df["date"] == d)].sum() for d in dates]
+            for c_1 in cc_list
         }
+        for_plot_2_true.update({
+            "СПГ": [lng_df["flow_true"][lng_df["date"] == d].sum() for d in dates]
+        })
+        for_plot_2_pred = {
+            c_1: [export_df["flow_pred"][(export_df["c_1"] == c_1) & (export_df["date"] == d)].sum() for d in dates]
+            for c_1 in cc_list
+        }
+        for_plot_2_pred.update({
+            "СПГ": [lng_df["flow_pred"][lng_df["date"] == d].sum() for d in dates]
+        })
 
-        for cc_i in cc_list[:-1]:
-            month_pred = for_plot_2_pred[cc_i]
-            month_true = for_plot_2_true[cc_i]
-            plt.plot(dates, month_pred, label="Данные")
-            plt.plot(dates, month_true, label="Рассчет")
+        for cc_i in cc_list:
 
+            plt.plot(dates, for_plot_2_true[cc_i], label="Данные")
+            plt.plot(dates, for_plot_2_pred[cc_i], label="Рассчет")
+            plt.title(cc_i)
             plt.legend()
             plt.show()
 
 
-
-
-    visual(2019, "out/res_2019_mode_1.json", {"DZ", "RU", "NO"})
+    year = 2019
+    visual(year, f"out/res_{year}_mode_0_2m.json", {"DZ", "RU", "NO", "AZ", "LY", "IR"})
